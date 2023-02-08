dcef729ed1f8c7cf9a58923f9d3243224358f25b
Raihan Khalil Abdillah <tatuhey@gmail.com>
2023-02-08 11:08:41 +0800

    Add files via upload

---
+++ CatASCIIArt.py
@@ -0,0 +1,15 @@
+# Raihan Khalil Abdillah, 30065695
+# AT2.1 Question 2
+# Cat ASCII art
+
+print("""
+            __..--''``---....___   _..._    __
+ /// //_.-'    .-/";  `        ``<._  ``.''_ `. / // /
+///_.-' _..--.'_    \                    `( ) ) // //
+/ (_..-' // (< _     ;_..__               ; `' / ///
+ / // // //  `-._,_)' // / ``--...____..-' /// / //
+          """)  # taken from https://www.asciiart.eu/animals/cats
+
+print("|\---/|")
+print("| o_o |")
+print(" \_^_/")     #taken from https://www.asciiart.eu/animals/cats
---
+++ HelloWorld.py
@@ -0,0 +1,5 @@
+# Raihan Khalil Abdillah, 30065695
+# AT2.1 Question 1
+# Hello World
+
+print("Hello World")
